27617b25666ac6483089e271b8572dfe14266eea
Paulo Augusto <paulo-augusto12@outlook.com>
2022-10-02 18:14:31 -0300

    subindo jogo

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const loop = setInterval(()=> {
         mario.style.bottom= `${marioPosition}px`
 
 
-        mario.src = '../Assets/game-over.png'
+        mario.src = '../images/game-over.png'
         mario.style.width = '75px';
         mario.style.marginLeft = '50px'
 
